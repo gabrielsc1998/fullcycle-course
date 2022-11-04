c59c4bf359e9fbb912c236b156313fcd888d622d
gabrielsc1998 <98gabrielsc@gmail.com>
2022-11-04 10:45:44 -0300

    refactor: change data type of event

--- ddd/patterns/src/domain/customer/event/customer-changed-address.event.ts
+++ ddd/patterns/src/domain/customer/event/customer-changed-address.event.ts
@@ -1,10 +1,18 @@
 import EventInterface from "../../@shared/event/event.interface";
 
+import Address from "../value-object/address";
+
+export type CustomerChangedAddressEventData = {
+  id: string;
+  name: string;
+  address: Address;
+};
+
 export default class CustomerChangedAddressEvent implements EventInterface {
   dataTimeOccurred: Date;
-  eventData: any;
+  eventData: CustomerChangedAddressEventData;
 
-  constructor(eventData: any) {
+  constructor(eventData: CustomerChangedAddressEventData) {
     this.dataTimeOccurred = new Date();
     this.eventData = eventData;
   }
--- ddd/patterns/src/domain/customer/event/test/customer-changed-address.event.spec.ts
+++ ddd/patterns/src/domain/customer/event/test/customer-changed-address.event.spec.ts
@@ -1,17 +1,26 @@
-import CustomerChangedAddressEvent from "../customer-changed-address.event";
+import Address from "../../value-object/address";
 
-const makeCustomerChangedAddressEvent = (
-  data: any
-): CustomerChangedAddressEvent => {
-  return new CustomerChangedAddressEvent(data);
+import CustomerChangedAddressEvent, {
+  CustomerChangedAddressEventData,
+} from "../customer-changed-address.event";
+
+const mockCustomerChangedAddressData: CustomerChangedAddressEventData = {
+  id: "customer-id",
+  name: "customer-name",
+  address: new Address("street", 1, "zip", "city"),
+};
+
+const makeCustomerChangedAddressEvent = (): CustomerChangedAddressEvent => {
+  return new CustomerChangedAddressEvent(mockCustomerChangedAddressData);
 };
 
 describe("Customer Changed Address Event", () => {
   it("should create an event", () => {
-    const customerChangedAddressEvent =
-      makeCustomerChangedAddressEvent("any-data");
+    const customerChangedAddressEvent = makeCustomerChangedAddressEvent();
     expect(customerChangedAddressEvent).toBeDefined();
-    expect(customerChangedAddressEvent.eventData).toEqual("any-data");
+    expect(customerChangedAddressEvent.eventData).toEqual(
+      mockCustomerChangedAddressData
+    );
     expect(customerChangedAddressEvent.dataTimeOccurred).toBeDefined();
   });
 });
