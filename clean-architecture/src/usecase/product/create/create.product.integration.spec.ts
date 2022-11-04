592f3929421ed63eb54c2552dc7d3bbdb90fdb99
gabrielsc1998 <98gabrielsc@gmail.com>
2022-11-04 16:06:25 -0300

    feat: implements create product use case

---
+++ clean-architecture/src/usecase/product/create/create.product.integration.spec.ts
@@ -0,0 +1,79 @@
+import { DBSequelizeConnection } from "../../../common/test/db/db-sequelize.connection";
+import ProductModel from "../../../infrastructure/product/repository/sequelize/product.model";
+import ProductRepository from "../../../infrastructure/product/repository/sequelize/product.repository";
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+
+import CreateProductUseCase from "./create.product.usecase";
+
+type SUT = {
+  repository: ProductRepositoryInterface;
+  useCase: CreateProductUseCase;
+};
+
+const makeSut = (): SUT => {
+  const productRepository = new ProductRepository();
+  const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+  return {
+    repository: productRepository,
+    useCase: productCreateUseCase,
+  };
+};
+
+let sut: SUT = null;
+
+const input = {
+  name: "computer",
+  price: 200,
+};
+
+describe("Create Product Use Case  [ integration ]", () => {
+  let dbClient: DBSequelizeConnection;
+
+  beforeAll(() => (sut = makeSut()));
+
+  beforeEach(async () => {
+    dbClient = new DBSequelizeConnection();
+    await dbClient.initialize([ProductModel]);
+  });
+
+  afterEach(async () => {
+    await dbClient.close();
+  });
+
+  it("should create a product", async () => {
+    const output = await sut.useCase.execute(input);
+
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+
+    const storedProduct = await sut.repository.find(output.id);
+
+    expect(output.id).toEqual(storedProduct.id);
+    expect(output.name).toEqual(storedProduct.name);
+    expect(output.price).toEqual(storedProduct.price);
+  });
+
+  it("should thrown an error when name is missing", async () => {
+    const spyRepository = jest.spyOn(sut.repository, "create");
+
+    await expect(sut.useCase.execute({ ...input, name: "" })).rejects.toThrow(
+      "Name is required"
+    );
+
+    expect(spyRepository).not.toBeCalled();
+  });
+
+  it("should thrown an error when the price is less than zero", async () => {
+    const spyRepository = jest.spyOn(sut.repository, "create");
+
+    await expect(sut.useCase.execute({ ...input, price: -1 })).rejects.toThrow(
+      "Price must be greater than zero"
+    );
+
+    expect(spyRepository).not.toBeCalled();
+  });
+});
---
+++ clean-architecture/src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,59 @@
+import ProductRepositoryInterface from "../../../domain/product/repository/product-repository.interface";
+
+import CreateProductUseCase from "./create.product.usecase";
+
+const MockRepository = () => {
+  return {
+    find: jest.fn(),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+type SUT = {
+  repository: ProductRepositoryInterface;
+  useCase: CreateProductUseCase;
+};
+
+const makeSut = (): SUT => {
+  const productRepository = MockRepository();
+  const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+  return {
+    repository: productRepository,
+    useCase: productCreateUseCase,
+  };
+};
+
+let sut: SUT = null;
+
+const input = {
+  name: "computer",
+  price: 200,
+};
+
+describe("Create Product Use Case [ unity ]", () => {
+  beforeAll(() => (sut = makeSut()));
+
+  it("should create a product", async () => {
+    const output = await sut.useCase.execute(input);
+    expect(output).toEqual({
+      id: expect.any(String),
+      name: input.name,
+      price: input.price,
+    });
+  });
+
+  it("should thrown an error when name is missing", async () => {
+    await expect(sut.useCase.execute({ ...input, name: "" })).rejects.toThrow(
+      "Name is required"
+    );
+  });
+
+  it("should thrown an error when the price is less than zero", async () => {
+    await expect(sut.useCase.execute({ ...input, price: -1 })).rejects.toThrow(
+      "Price must be greater than zero"
+    );
+  });
+});
